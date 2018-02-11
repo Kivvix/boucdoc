e6cc91c60de819064f924f377f2175eb4d3cb154
Josselin <josselin.massot@gmail.com>
2018-02-11 19:18:27 +0100

    :tada: Plein de trucs d'un coup car j'ai jamais commité pendant mon dev (pas bien)

---
+++ filter/panviz/main.py
@@ -0,0 +1,54 @@
+#! /usr/bin/env python
+
+import panflute as pf
+import io
+import subprocess
+import uuid
+
+def tmp_file ( filename,elem ) :
+	"""
+		write elem content in temporary file with filename name
+		I use io module because of UTF-8 encoding
+	"""
+	tmp_f = io.open(filename,'w',encoding='utf8')
+	tmp_f.write(pf.stringify(elem))
+	tmp_f.close()
+
+
+def action ( elem,doc ) :
+	"""
+		action of filter, get all codeblock of class `dot` and replace it with a figure
+	"""
+	if isinstance(elem,pf.CodeBlock) and u'dot' in elem.classes :
+
+		renderer = u'dot'
+		if u'renderer' in elem.attributes :
+			renderer = elem.attributes[u'renderer']
+
+		f = u'png'
+		if doc.format == 'latex' :
+			f = u'eps'
+		elif u'format' in elem.attributes :
+			f = elem.attributes[u'format']
+
+		folder = '/tmp'
+		if doc.format == 'html' :
+			folder = './build/html'
+
+		gv_in  = "/tmp/panviz_"+str(uuid.uuid4())+".gv"
+		image  = "panviz_"+str(uuid.uuid4())+"."+f
+		gv_out = folder + "/" + image
+		tmp_file( gv_in , elem )
+
+		subprocess.check_call(" ".join([renderer,"-T"+f,gv_in,"-o",gv_out]),shell=True)
+		raw_title = pf.stringify(elem).split('\n',1)[0][1:]
+		title = pf.convert_text(raw_title)
+		title = (title[0]).content
+		return pf.Para(pf.Image(*title,url=image,title=u'fig:'+raw_title))
+
+def main(doc=None):
+	return pf.run_filter(action,doc=doc)
+
+if __name__ == '__main__':
+	main()
+
